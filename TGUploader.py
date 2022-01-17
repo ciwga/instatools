f24d0bcd8a46c0c7b3918c4990c1a616133ca323
Mustafa <42353725+ciwga@users.noreply.github.com>
2022-01-17 22:20:41 +0300

    1701

--- TGUploader.py
+++ TGUploader.py
@@ -1,116 +1,113 @@
 from time import sleep
+from mkinf import sha256, timestamp2date
 import sqlite3
 import requests
 import os
 import sys
+import re
 
 
-tgdb = sqlite3.connect("tg_log.db")
-cursor = tgdb.cursor()
-z = {}
+class Database():
+    def __init__(self, name='tGlog.db', directory=os.getcwd()):
+        self.directory = directory
+        self.name = name
+        path = os.path.join(self.directory, self.name)
+        self.dtb = sqlite3.connect(path)
+        self.cur = self.dtb.cursor()
+        self.cur.execute('''CREATE TABLE IF NOT EXISTS UPFile'''
+                         '''(file_sha256 text, file_name text,\
+                             uploaded_time text)''')
+        self.dtb.commit()
+
+    def db_insert(self, fhash, name, time):
+        self.cur.execute('''INSERT INTO UPFile VALUES (?,?,?)''',
+                         [fhash, name, time])
+        self.dtb.commit()
+
+    def db_check(self, data):
+        self.cur.execute('''SELECT * FROM UPFile WHERE file_sha256 = ?''',
+                         [data])
+        result = self.cur.fetchall()
+        return len(result)
 
 
-def db_create():
-    cursor.execute('''CREATE TABLE IF NOT EXISTS Uploaded_Files'''
-                   '''(file_name text)''')
-    tgdb.commit()
+class TelegramBasicUpload(Database):
 
+    def __init__(self):
+        super().__init__()
+        self.token = "enter token here"
+        self.api = "https://api.telegram.org/bot"+self.token
+        self.url = self.api+"/getUpdates"
 
-db_create()
+        self.r = requests.get(self.url)
+        if self.r.status_code == 404:
+            print("ERROR:\nToken invalid (Line 38) or",
+                  "no internet connection!!")
+            sys.exit()
 
+    def FileUpload(self, directory):
+        ph = "/sendPhoto"
+        vd = "/sendVideo"
+        cp = "&caption="
+        chvalue = ""    # Enter telegram chat id
 
-def db_insert(value):
-    cursor.execute('''INSERT INTO Uploaded_Files VALUES (?)''', [value])
-    tgdb.commit()
-
-
-def db_check(data):
-    cursor.execute('''SELECT * FROM Uploaded_Files WHERE file_name = ?''',
-                   [data])
-    result = cursor.fetchall()
-    return len(result)
-
-
-def upload(dirn):
-    token = ""  # "telegram_bot_token"
-    api = "https://api.telegram.org/bot"+token
-    url = api+"/getUpdates"
-
-    r = requests.get(url)
-    if r.status_code == 404:
-        print("ERROR:\nToken invalid or no internet connection!!")
-        sys.exit()
-    else:
-        jr = r.json()
-        r_list = jr['result']
-        if len(r_list) > 1:
-            for index in range(0, len(r_list)):
-                try:
-                    i = r_list[index]['message']['chat']['id']
-                    title = r_list[index]['message']['chat']['title']
-                    z[title] = i
-                except KeyError:
-                    try:
-                        i = r_list[index]['channel_post']['chat']['id']
-                        title = r_list[index]['channel_post']['chat']['title']
-                        z[title] = i
-                    except KeyError:
-                        pass
-            if z is None:
-                print("Send a message to channel/group and try again!!")
+        if not chvalue:
+            resp = self.r.json()
+            result = resp['result']
+            regx = re.compile('-[0-9]+')
+            values = regx.search(str(result))
+            title = result[0]['message']['chat']['title']
+            try:
+                chvalue = values.group(0)
+                print(f"Uploading to {title}...")
+            except IndexError:
+                print("Enter Telegram Chat ID (line 51)")
                 sys.exit()
-            print(z)
-            channel_name = str(input("Enter the Channel/Group Name: "))
-            chid_value = z.get(channel_name)
-        else:
-            if len(r_list) == 0:
-                print(" TIP:\nSend a message to group involved the bot ",
-                      "or disable privacy mode using bot father")
-                sys.exit()
-            else:
-                try:
-                    chid_value = r_list[0]['message']['chat']['id']
-                except KeyError:
-                    chid_value = r_list[0]['channel_post']['chat']['id']
 
-    ph = "/sendPhoto"
-    vd = "/sendVideo"
-    cp = "&caption="
-    ch_id = "?chat_id="+str(chid_value)
+        ch_id = "?chat_id="+str(chvalue)
+        for root, dirs, filenames in os.walk(directory):
+            for filename in filenames:
+                dirname = os.path.dirname(root+"/"+filename)
+                files = os.path.join(dirname, filename)
+                t_name = os.path.splitext(files)[0]
 
-    for root, dirs, filenames in os.walk(dirn):
-        for filename in filenames:
-            dirname = os.path.dirname(root+"/"+filename)
-            t_name = os.path.splitext(dirname+"/"+filename)[0]
-            sub = filename
-            if db_check(filename) == 0:
-                if os.path.isfile(t_name+".txt"):
-                    with open(t_name+".txt", "r", encoding="utf-8") as c:
-                        for line in c:
-                            if not line:
-                                break
-                            sub = line
-                if filename.endswith('jpg'):
-                    with open(f"{dirname}/{filename}", 'rb') as image:
-                        link = f"{api}{ph}{ch_id}{cp}{sub}"
-                        requests.post(link, files={'photo': image})
-                        print(f"{filename} was uploaded to telegram")
-                        db_insert(filename)
-                        sleep(1.89)
-                elif filename.endswith("mp4"):
-                    with open(f"{dirname}/{filename}", 'rb') as video:
-                        link = f"{api}{vd}{ch_id}{cp}{sub}"
-                        requests.post(link, files={'video': video})
-                        print(f"{filename} was uploaded to telegram")
-                        db_insert(filename)
-                        sleep(1.89)
+                sub = filename
+                if self.db_check(sha256(files)) == 0:
+                    if os.path.isfile(t_name+".txt"):
+                        with open(t_name+".txt", "r", encoding="utf-8") as c:
+                            for line in c:
+                                if not line:
+                                    break
+                                sub = line
+                    if filename.endswith('jpg'):
+                        with open(files, 'rb') as image:
+                            link = f"{self.api}{ph}{ch_id}{cp}{sub}"
+                            requests.post(link, files={'photo': image})
+                            print(f"{filename} was uploaded to telegram")
+                            self.db_insert(sha256(files), filename,
+                                           timestamp2date())
+                            sleep(1.89)
+                    elif filename.endswith("mp4"):
+                        with open(files, 'rb') as video:
+                            link = f"{self.api}{vd}{ch_id}{cp}{sub}"
+                            requests.post(link, files={'video': video})
+                            print(f"{filename} was uploaded to telegram")
+                            self.db_insert(sha256(files), filename,
+                                           timestamp2date())
+                            sleep(1.89)
+                    else:
+                        os.remove(files)
                 else:
-                    os.remove(dirname+"/"+filename)
-            else:
-                print(f"{filename} already uploaded to telegram, " +
-                      "Deleted !!")
-                os.remove(dirname+"/"+filename)
+                    print(f"{filename} already uploaded to telegram, " +
+                          "Deleted !!")
+                    os.remove(files)
+        self.dtb.close()
 
 
-if __name__ == "__main__":
-    tgdb.close()
+def main():
+    cl = TelegramBasicUpload()
+    cl.FileUpload('Instagram_Data')
+
+
+if __name__ == '__main__':
+    main()
